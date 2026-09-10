1028224bd1f0b534a83567fe659048e5805d8abd
ramiz <ramiz.ozdemir@live.acibadem.edu.tr>
2026-09-10 19:22:32 +0300

    Butun klavyeler buyutuldu: alistirmalar, Ailecuk, oyun, yaris ve final

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 /* Fiko: cevrimdisi calisma katmani. Otomatik uretildi, elle duzenleme.
-   Uretim: 2026-09-10 19:03 */
-const SURUM = "fiko-7933380af3";
+   Uretim: 2026-09-10 19:22 */
+const SURUM = "fiko-709b560634";
 const DOSYALAR = [
   "./",
   "4harf.html",
