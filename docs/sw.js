1ddf202e6a6729075aa6bc6e9226316b3ef15dac
rmz-oz <ozdemir.ramsey@gmail.com>
2026-09-11 12:20:19 +0300

    Papatyada sari yaprak iki kat puan kurali kaldirildi

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 /* Fiko: cevrimdisi calisma katmani. Otomatik uretildi, elle duzenleme.
-   Uretim: 2026-09-11 12:17 */
-const SURUM = "fiko-322a05826b";
+   Uretim: 2026-09-11 12:20 */
+const SURUM = "fiko-49adb6453c";
 const DOSYALAR = [
   "./",
   "4harf.html",
